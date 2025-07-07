c1de058e11a567bab710675d994f8122bc7bb07f
Jonathan Orme <jonathan.orme@quakercolor.com>
2025-07-07 14:26:03 -0400

    Add custom clicked signal for SquareButton

--- src/gui/square_button.py
+++ src/gui/square_button.py
@@ -2,6 +2,7 @@
 
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSignal
 from src.models.square import Square
 
 ICONS = [
@@ -14,10 +15,23 @@ ICONS = [
 class SquareButton(QPushButton):
     """View/controller widget of an individual square."""
 
-    def __init__(self, square: Square):
+    clicked_coord = pyqtSignal(int, int)
+    """Emitted when clicked, carries coordinates of self on the board."""
+
+    def __init__(self, square: Square, row: int, col: int):
+        """Initialize the SqaureButton widget.
+        
+        Args:
+            square (Square): The model for the square.
+            row (int): The row of the square on the board.
+            col (int): The column of the square on the board.
+        """
         super().__init__()
         self.square = square
         self.update_state()
+        self.clicked.connect(lambda: self.clicked_coord.emit(row, col))
     
     def update_state(self):
+        """Match state with that of the model."""
         self.setIcon(ICONS[self.square.state])
+        self.setEnabled(not self.square.state)
--- tests/test_square_button.py
+++ tests/test_square_button.py
@@ -14,14 +14,16 @@ def setup_module():
 def test_square_button_creation():
     """Test the creation of a SquareButton."""
     square = Square()
-    square_button = SquareButton(square)
+    square_button = SquareButton(square, 0, 0)
     assert square_button.square == square
 
 def test_square_button_state_change():
     """Test to make sure the buttons shows the state of the square."""
     square = Square()
-    square_button = SquareButton(square)
+    square_button = SquareButton(square, 0, 0)
     assert square_button.icon().isNull()
+    assert square_button.isEnabled()
     square_button.square.mark_x()
     square_button.update_state()
     assert not square_button.icon().isNull()
+    assert not square_button.isEnabled()
