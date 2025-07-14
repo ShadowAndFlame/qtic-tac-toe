327c79a22a27d150f400e34b9155fc0aead42c76
Jonathan Orme <jonathan.orme@quakercolor.com>
2025-07-14 09:00:50 -0400

    Add dummy robot move (literally a dummy, chooses first option)

--- src/models/player.py
+++ src/models/player.py
@@ -1,6 +1,7 @@
 """Model for a player, whether human or computer."""
 
 from enum import IntEnum
+from src.models import board
 
 class PlayerSymbol(IntEnum):
     """The symbol the player marks on the board.
@@ -26,3 +27,14 @@ class Robot(Player):
     def __init__(self):
         super().__init__()
         self.robot = True
+
+    def best_move(self, board: 'board.Board') -> tuple[int, int]:
+        """Choose the best move to make given the board state.
+        
+        Args:
+            board (Board): The current board state. Need not include self.
+
+        Returns:
+            tuple[int, int]: The row and column to make a move in."""
+        possible_moves = board.empty_square_coords
+        return possible_moves[0]
--- tests/test_player.py
+++ tests/test_player.py
@@ -2,6 +2,7 @@
 
 import pytest
 from src.models.player import Player, Robot, PlayerSymbol
+from src.models.board import Board
 
 def test_player_creation():
     """Test the creation of a player."""
@@ -14,3 +15,10 @@ def test_robot_creation():
     player = Robot()
     assert player.symbol == None
     assert player.robot == True
+
+def test_robot_choose_move():
+    """Test the automatic move selection by a robot."""
+    robot = Robot()
+    board = Board(robot, Player())
+    move = robot.best_move(board)
+    assert move in board.empty_square_coords
